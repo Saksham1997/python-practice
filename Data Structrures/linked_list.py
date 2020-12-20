1c5dc7c3395aca7b701c4534bbfabba4c40a7780
Saksham Singhal <sakshamsinghal08@gmail.com>
2020-12-20 23:51:48 +0530

    index_at_pos_added

--- Data Structrures/linked_list.py
+++ Data Structrures/linked_list.py
@@ -1,3 +1,4 @@
+import pdb
 class node:
     def __init__(self,data=None):
         self.data=data
@@ -22,12 +23,34 @@ class linked_list:
             cur_node = cur_node.next
             lis.append(cur_node.data)
         print(lis)
-"""
+
+    def length(self):
+        cur=self.head
+        length=0
+        while cur.next!=None:
+            cur=cur.next
+            length+=1
+        return length
+
     def index_pos(self,index):
         if index>=self.length() or index<0:
             print ("ERROR:index out of range")
             return None
-"""
+        cur_index=0
+        cur_node=self.head
+        for i in range(0,index):
+            cur_node = cur_node.next
+        return cur_node.data
+
+    def insert_at_pos(self,index,data):
+        print("The Length of the list currently is {}".format(self.length()))
+        cur=self.head
+        for i in range(0,index-1):
+            cur=cur.next
+        #pdb.set_trace()
+        new_node=node(data)
+        new_node.next = cur.next
+        cur.next=new_node
 
 
 my_list = linked_list()
@@ -37,4 +60,7 @@ my_list.append(2)
 my_list.append(3)
 my_list.append(4)
 my_list.append(5)
+my_list.display()
+print("the number at 2 is {}".format(my_list.index_pos(2)))
+my_list.insert_at_pos(2,70)
 my_list.display()
